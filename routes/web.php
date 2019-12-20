df7b27d361e603f850941eaee97a16e62045d8d6
Lapua <c011731950@edu.teu.ac.jp>
2019-12-20 16:56:04 +0900

    auth

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Auth::routes();
 
 /***   ***/
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/sales', function () {
     return view('sales');
