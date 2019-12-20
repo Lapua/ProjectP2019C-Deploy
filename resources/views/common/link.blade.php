ceab45cd2eacc52a56f3a1f580070b090acd99e6
Lapua <c011731950@edu.teu.ac.jp>
2019-12-20 15:16:01 +0900

    first

---
+++ resources/views/common/link.blade.php
@@ -0,0 +1,1 @@
+<button class="w-56 h-32 bg-blue-200 m-10 text-xl font-bold text-gray-700 border-2 border-gray-700" onclick="location.href='{{ route($link) }}'">{{ $title }}</button>
---
+++ resources/views/sales.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<img src="{{ asset('images/sales-graph.png') }}">
+@endsection
