d2a38951668eb944f6b4ddff4a75c0dc9abd9fb7
Lapua <c011731950@edu.teu.ac.jp>
2020-01-10 14:41:39 +0900

    sales

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,13 +12,13 @@
     @endif
 
         <div class="row justify-around">
-            @include('common.link', ['title' => '売上確認', 'link' => 'sales'])
-            @include('common.link', ['title' => '在庫管理', 'link' => 'sales'])
-            @include('common.link', ['title' => 'ファイルアップロード', 'link' => 'sales'])
+            @include('common.link', ['title' => '売上確認', 'detail' => '売り上げの確認', 'link' => 'sales'])
+            @include('common.link', ['title' => '在庫管理', 'detail' => '在庫の管理', 'link' => 'sales'])
+            @include('common.link', ['title' => 'ファイルアップロード', 'detail' => '領収書やレシートの送信', 'link' => 'sales'])
         </div>
         <div class="row justify-around">
-            @include('common.link', ['title' => 'コンサルティング', 'link' => 'sales'])
-            @include('common.link', ['title' => 'セミナー', 'link' => 'sales'])
-            @include('common.link', ['title' => 'コミュニティ', 'link' => 'sales'])
+            @include('common.link', ['title' => 'コンサルティング', 'detail' => 'ビジネス戦略のアドバイス', 'link' => 'sales'])
+            @include('common.link', ['title' => 'セミナー', 'detail' => 'セミナーをチェック', 'link' => 'sales'])
+            @include('common.link', ['title' => 'コミュニティ', 'detail' => '他会員とコミュニケーション', 'link' => 'sales'])
         </div>
 @endsection
--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-gray-700 text-xl pb-2">売上</h1>
-    <div class="h-1 w-full bg-blue-500 mb-10"></div>
-    <img src="{{ asset('images/sales-graph.png') }}">
+    <div class="m-4">
+        <h1 class="text-gray-700 text-xl pb-2">売上</h1>
+        <div class="h-1 w-full bg-blue-500 mb-10"></div>
+        <div class="flex">
+            <img src="{{ asset('images/sales-graph.png') }}" class="w-3/4">
+            <div class="bg-blue-300 w-1/4 mx-4 flex items-center">
+                <div class="m-3">ここにコンサルティング内容が入ります</div>
+            </div>
+        </div>
+    </div>
 @endsection
